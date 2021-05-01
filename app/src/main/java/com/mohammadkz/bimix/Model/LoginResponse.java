c866d608f4158e9b5c369a75729d2b90cda9de9f
mohammadmehdi khajehzadeh <mohammad.mehdi.khz@gmail.com>
2021-05-01 17:19:55 +0430

    user class and login response  are created

---
+++ app/src/main/java/com/mohammadkz/bimix/Model/LoginResponse.java
@@ -0,0 +1,49 @@
+package com.mohammadkz.bimix.Model;
+
+public class LoginResponse {
+
+    private String state, code, ID, name, auth;
+
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuth() {
+        return auth;
+    }
+
+    public void setAuth(String auth) {
+        this.auth = auth;
+    }
+
+}
+
---
+++ app/src/main/java/com/mohammadkz/bimix/Model/User.java
@@ -0,0 +1,88 @@
+package com.mohammadkz.bimix.Model;
+
+public class User {
+    private String ID, auth, name, password, idCardImage, birthdayDate, phoneNumber, idCard;
+
+    public User(String ID, String auth, String name, String phoneNumber) {
+        this.ID = ID;
+        this.auth = auth;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public User() {
+
+    }
+
+    public User(String name, String password, String phoneNumber) {
+        this.name = name;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getAuth() {
+        return auth;
+    }
+
+    public void setAuth(String auth) {
+        this.auth = auth;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getIdCardImage() {
+        return idCardImage;
+    }
+
+    public void setIdCardImage(String idCardImage) {
+        this.idCardImage = idCardImage;
+    }
+
+    public String getBirthdayDate() {
+        return birthdayDate;
+    }
+
+    public void setBirthdayDate(String birthdayDate) {
+        this.birthdayDate = birthdayDate;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getIdCard() {
+        return idCard;
+    }
+
+    public void setIdCard(String idCard) {
+        this.idCard = idCard;
+    }
+
+
+}
