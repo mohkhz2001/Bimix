88e42d4585b00a60602ceb3b7dfb273bd95d476a
mohammadmehdi khajehzadeh <mohammad.mehdi.khz@gmail.com>
2021-05-01 14:58:48 +0430

    choose login/sign up option start applicaion

---
+++ app/src/main/java/com/mohammadkz/bimix/Activity/StartUpActivity.java
@@ -0,0 +1,18 @@
+package com.mohammadkz.bimix.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mohammadkz.bimix.R;
+
+public class StartUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_up);
+
+
+    }
+}
