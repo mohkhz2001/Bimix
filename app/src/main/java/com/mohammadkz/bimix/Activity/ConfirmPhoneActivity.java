b5c4175d09477ed6db57ec58f43c1bd81adb86f0
mohammadmehdi khajehzadeh <mohammad.mehdi.khz@gmail.com>
2021-05-02 12:39:59 +0430

    confirm phone number page

---
+++ app/src/main/java/com/mohammadkz/bimix/Activity/ConfirmPhoneActivity.java
@@ -0,0 +1,16 @@
+package com.mohammadkz.bimix.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mohammadkz.bimix.R;
+
+public class ConfirmPhoneActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirm_phone);
+    }
+}
