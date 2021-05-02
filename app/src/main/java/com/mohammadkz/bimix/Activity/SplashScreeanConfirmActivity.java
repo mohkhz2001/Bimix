aea7d7e57899842dfbfdf40b6271289fe4b4eab4
mohammadmehdi khajehzadeh <mohammad.mehdi.khz@gmail.com>
2021-05-02 13:05:00 +0430

    after confirm phone number this screen will be apear

---
+++ app/src/main/java/com/mohammadkz/bimix/Activity/SplashScreeanConfirmActivity.java
@@ -0,0 +1,16 @@
+package com.mohammadkz.bimix.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mohammadkz.bimix.R;
+
+public class SplashScreeanConfirmActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screean_confirm);
+    }
+}
