e16836fee0e53ae4ee57a0c3a8344faf3ef9e4d2
mohammadmehdi khajehzadeh <mohammad.mehdi.khz@gmail.com>
2021-05-04 00:14:13 +0430

    this is static class that shpw the alert dialog when have any error

--- app/src/main/java/com/mohammadkz/bimix/Activity/SplashScreenConfirmActivity.java
+++ app/src/main/java/com/mohammadkz/bimix/Activity/SplashScreenConfirmActivity.java
@@ -1,16 +1,32 @@
 package com.mohammadkz.bimix.Activity;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
 import com.mohammadkz.bimix.R;
 
-public class SplashScreeanConfirmActivity extends AppCompatActivity {
+public class SplashScreenConfirmActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screan_confirm);
+        setContentView(R.layout.activity_splash_screen_confirm);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreenConfirmActivity.this, MainPageActivity.class));
+                finish();
+            }
+        }, 2000);
     }
 }
