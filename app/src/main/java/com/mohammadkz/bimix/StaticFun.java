b984aedc70a784d5838f17b96e220059e8d4b582
mohammadmehdi khajehzadeh <mohammad.mehdi.khz@gmail.com>
2021-05-07 20:13:17 +0430

    useless fun removed

--- app/src/main/java/com/mohammadkz/bimix/StaticFun.java
+++ app/src/main/java/com/mohammadkz/bimix/StaticFun.java
@@ -6,8 +6,9 @@ import android.content.DialogInterface;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.mohammadkz.bimix.Activity.LoginActivity;
 
-public class ErrorHandler {
+public class StaticFun {
 
+    // error handler
     public static void alertDialog_connectionFail(Context context) {
         MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(context);
         builder.setTitle(R.string.connection_failed_title);
@@ -26,6 +27,7 @@ public class ErrorHandler {
         builder.show();
     }
 
+    // error handler for input wrong phone number or pass
     public static void alertDialog_error_login(Context context) {
         MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(context);
         builder.setTitle(R.string.faileLogin_title);
@@ -43,36 +45,5 @@ public class ErrorHandler {
 
         builder.show();
     }
-
-    public static int alertDialog_error_loadingPage(Context context) {
-        MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(context);
-        builder.setTitle(R.string.faileLogin_title);
-        builder.setMessage(R.string.faileLogin_text);
-        String positive = "تلاش مجدد";
-        String negetive = "بستن";
-        final int[] choose = {0};
-
-        // have one btn ==> try again
-        builder.setPositiveButton(positive, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                choose[0] = 1;
-                dialog.dismiss();
-            }
-        });
-        // have one btn ==> close
-        builder.setPositiveButton(negetive, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                choose[0] = 2;
-                dialog.dismiss();
-            }
-        });
-
-        builder.show();
-
-        return choose[0];
-    }
-
 
 }
