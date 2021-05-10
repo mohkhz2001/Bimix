b15c234b729e7df69e2b9efed1b945f582f13191
mohammadmehdi khajehzadeh <mohammad.mehdi.khz@gmail.com>
2021-05-11 04:20:27 +0430

    class for retrofit response

---
+++ app/src/main/java/com/mohammadkz/bimix/Model/RequestResponse.java
@@ -0,0 +1,21 @@
+package com.mohammadkz.bimix.Model;
+
+public class RequestResponse {
+    private String code , state;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+}
