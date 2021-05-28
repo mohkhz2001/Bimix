5901a99fa2dd3e8a08811b45d7d2418bd7d0ac8f
mohammadmehdi khajehzadeh <mohammad.mehdi.khz@gmail.com>
2021-05-28 17:33:31 +0430

    on click lisener added ==> get link  - pay the insurance  - set payed in DB

--- app/src/main/java/com/mohammadkz/bimix/Adapter/HistoryAdapter.java
+++ app/src/main/java/com/mohammadkz/bimix/Adapter/HistoryAdapter.java
@@ -13,13 +13,13 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.mohammadkz.bimix.Model.InsuranceResponse;
 import com.mohammadkz.bimix.R;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class HistoryAdapter extends RecyclerView.Adapter<HistoryAdapter.viewHolder> {
 
     private Context context;
     private List<InsuranceResponse> insuranceResponses;
+    public OnItemClickListener onPayItemClickListener, onPayedItemClickListener, onGetItemClickListener;
 
     public HistoryAdapter(Context context, List<InsuranceResponse> insuranceResponses) {
         this.context = context;
@@ -34,38 +34,63 @@ public class HistoryAdapter extends RecyclerView.Adapter<HistoryAdapter.viewHold
     }
 
     @Override
-    public void onBindViewHolder(@NonNull HistoryAdapter.viewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull viewHolder holder, int position) {
         holder.date_req.setText(insuranceResponses.get(position).getDate());
         holder.payCode.setText(insuranceResponses.get(position).getPayCode());
 
         holder.trackingCode.setText(insuranceResponses.get(position).getTrackingCode());
 
-        if (insuranceResponses.get(position).getStatus().equals("pending")) {
+        setKind(position, holder);
+        setVisibility(position, holder);
+
+    }
+
+    private void setKind(int pos, viewHolder holder) {
+        if (insuranceResponses.get(pos).getKind().equals("body_insurance")) {
+            holder.kind_txt.setText("بیمه بدنه");
+        } else if (insuranceResponses.get(pos).getKind().equals("third_party")) {
+            holder.kind_txt.setText("بیمه شخص ثالث");
+        }
+    }
+
+    private void setVisibility(int pos, viewHolder holder) {
+        if (insuranceResponses.get(pos).getStatus().equals("pending")) {
             holder.get.setVisibility(View.GONE);
             holder.payCode.setVisibility(View.GONE);
             holder.payCode_txt.setVisibility(View.GONE);
             holder.get_txt.setVisibility(View.GONE);
             holder.status.setText("در حال بررسی");
-        } else if (insuranceResponses.get(position).getStatus().equals("confirm")) {
+        } else if (insuranceResponses.get(pos).getStatus().equals("confirm")) {
             holder.get.setVisibility(View.VISIBLE);
             holder.payCode.setVisibility(View.VISIBLE);
             holder.payCode_txt.setVisibility(View.VISIBLE);
             holder.get_txt.setVisibility(View.VISIBLE);
             holder.status.setText("صادر شده");
-        } else if (insuranceResponses.get(position).getStatus().equals("rejected")) {
+        } else if (insuranceResponses.get(pos).getStatus().equals("rejected")) {
             holder.get.setVisibility(View.GONE);
             holder.payCode.setVisibility(View.GONE);
             holder.payCode_txt.setVisibility(View.GONE);
             holder.get_txt.setVisibility(View.GONE);
             holder.status.setText("رد شده");
-        } else if (insuranceResponses.get(position).getStatus().equals("payment")) {
+        } else if (insuranceResponses.get(pos).getStatus().equals("issuing")) {
+            holder.get.setVisibility(View.GONE);
+            holder.payCode.setVisibility(View.VISIBLE);
+            holder.payCode_txt.setVisibility(View.VISIBLE);
+            holder.get_txt.setVisibility(View.GONE);
+            holder.status.setText("در حال صدور");
+        } else if (insuranceResponses.get(pos).getStatus().equals("payment")) {
+            if (insuranceResponses.get(pos).getPayed() == null) {
+                holder.payed.setVisibility(View.VISIBLE);
+                holder.pay.setVisibility(View.VISIBLE);
+            } else {
+                holder.payed_txt.setVisibility(View.VISIBLE);
+            }
             holder.get.setVisibility(View.GONE);
             holder.payCode.setVisibility(View.VISIBLE);
             holder.payCode_txt.setVisibility(View.VISIBLE);
             holder.get_txt.setVisibility(View.GONE);
             holder.status.setText("منتظر پرداخت");
         }
-
     }
 
     @Override
@@ -75,8 +100,8 @@ public class HistoryAdapter extends RecyclerView.Adapter<HistoryAdapter.viewHold
 
     public class viewHolder extends RecyclerView.ViewHolder {
         TextView describe, status, trackingCode, date_req, payCode;
-        TextView payCode_txt, get_txt;
-        Button get;
+        TextView payCode_txt, get_txt, kind_txt, payed_txt;
+        Button get, payed, pay;
 
         public viewHolder(@NonNull View itemView) {
             super(itemView);
@@ -88,8 +113,48 @@ public class HistoryAdapter extends RecyclerView.Adapter<HistoryAdapter.viewHold
             this.get = itemView.findViewById(R.id.get);
             this.payCode_txt = itemView.findViewById(R.id.payCode_txt);
             this.get_txt = itemView.findViewById(R.id.get_txt);
+            this.kind_txt = itemView.findViewById(R.id.kind_txt);
+            this.payed = itemView.findViewById(R.id.payed);
+            this.pay = itemView.findViewById(R.id.pay);
+            this.payed_txt = itemView.findViewById(R.id.payed_txt);
+
+            payed.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    onPayedItemClickListener.onItemClick(getAdapterPosition(), v);
+                }
+            });
+
+            get.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    onGetItemClickListener.onItemClick(getAdapterPosition(), v);
+                }
+            });
+            pay.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    onPayItemClickListener.onItemClick(getAdapterPosition(), v);
+                }
+            });
 
         }
+    }
+
+    public void setOnPayedItemClickListener(OnItemClickListener onPayedItemClickListener) {
+        this.onPayedItemClickListener = onPayedItemClickListener;
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(int pos, View v);
+    }
+
+    public void setOnGetItemClickListener(OnItemClickListener onGetItemClickListener) {
+        this.onGetItemClickListener = onGetItemClickListener;
+    }
+
+    public void setOnPayItemClickListener(OnItemClickListener onPayItemClickListener) {
+        this.onPayItemClickListener = onPayItemClickListener;
     }
 
 }
--- app/src/main/java/com/mohammadkz/bimix/Fragment/HistoryFragment.java
+++ app/src/main/java/com/mohammadkz/bimix/Fragment/HistoryFragment.java
@@ -1,9 +1,15 @@
 package com.mohammadkz.bimix.Fragment;
 
 import android.app.ProgressDialog;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -11,16 +17,20 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.mohammadkz.bimix.API.ApiConfig;
 import com.mohammadkz.bimix.API.AppConfig;
+import com.mohammadkz.bimix.Activity.MainPageActivity;
 import com.mohammadkz.bimix.Adapter.HistoryAdapter;
 import com.mohammadkz.bimix.Model.HistoryInsuranceResponse;
 import com.mohammadkz.bimix.Model.InsuranceResponse;
+import com.mohammadkz.bimix.Model.UpdateResponse;
 import com.mohammadkz.bimix.Model.User;
 import com.mohammadkz.bimix.R;
 
-import java.util.ArrayList;
+
 import java.util.List;
 
 import retrofit2.Call;
@@ -90,10 +100,80 @@ public class HistoryFragment extends Fragment {
     }
 
     private void setAdapter(List<InsuranceResponse> insuranceResponseList) {
+
         HistoryAdapter historyAdapter = new HistoryAdapter(getContext(), insuranceResponseList);
         list.setHasFixedSize(false);
         list.setLayoutManager(new LinearLayoutManager(view.getContext()));
         list.setAdapter(historyAdapter);
+
+        // payed
+        historyAdapter.setOnPayedItemClickListener(new HistoryAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(int pos, View v) {
+
+                updatePayStatus(insuranceResponseList.get(pos));
+                refresh();
+            }
+        });
+
+        // get the download link
+        historyAdapter.setOnGetItemClickListener(new HistoryAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(int pos, View v) {
+
+            }
+        });
+
+        // press pay
+        historyAdapter.setOnPayItemClickListener(new HistoryAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(int pos, View v) {
+
+                copy(insuranceResponseList.get(pos).getPayCode());
+                Toast.makeText(getContext(), "شناسه پرداخت کپی شد.", Toast.LENGTH_LONG).show();
+                payFragment();
+
+
+            }
+        });
+
+    }
+
+    private void copy(String code) {
+        ClipboardManager clipboard = (ClipboardManager) getActivity().getSystemService(Context.CLIPBOARD_SERVICE);
+        ClipData clip = ClipData.newPlainText("paycode", code);
+        clipboard.setPrimaryClip(clip);
+    }
+
+    private void payFragment() {
+        FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+        PayFragment payFragment = new PayFragment(user);
+        fragmentTransaction.replace(R.id.frameLayout, payFragment).commit();
+
+        ((MainPageActivity) getActivity()).setTitle("پرداخت بیمه نامه");
+        ((MainPageActivity) getActivity()).setNavSelected(R.id.nav_pay);
+    }
+
+    private void updatePayStatus(InsuranceResponse insuranceResponse) {
+
+        Call<UpdateResponse> get = request.payStatus(user.getAuth(), "payed", insuranceResponse.getTrackingCode(), insuranceResponse.getKind());
+
+        get.enqueue(new Callback<UpdateResponse>() {
+            @Override
+            public void onResponse(Call<UpdateResponse> call, Response<UpdateResponse> response) {
+                Log.e("re", " " + response.body().getCode());
+            }
+
+            @Override
+            public void onFailure(Call<UpdateResponse> call, Throwable t) {
+                Log.e("re", " " + t.getMessage());
+            }
+        });
+
+    }
+
+    private void refresh() {
+        getData();
     }
 
 }
