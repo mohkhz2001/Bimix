7a1940157083b30c37717d9b71bb780cd5e3b41a
mohammadmehdi khajehzadeh <mohammad.mehdi.khz@gmail.com>
2021-05-28 17:29:06 +0430

    ip changed

--- app/src/main/java/com/mohammadkz/bimix/API/AppConfig.java
+++ app/src/main/java/com/mohammadkz/bimix/API/AppConfig.java
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class AppConfig {
     private static Retrofit retrofit;
-    public static final String url="http://192.168.1.118/insurance/";
+    public static final String url = "http://192.168.1.103/insurance/";
 
     public static Retrofit getRetrofit() {
         if (retrofit == null) {
