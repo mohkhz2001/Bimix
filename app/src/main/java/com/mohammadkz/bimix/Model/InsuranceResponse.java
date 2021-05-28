0928a5228239d2fa4e576c4a8616fd4f653f1d8a
mohammadmehdi khajehzadeh <mohammad.mehdi.khz@gmail.com>
2021-05-28 16:59:55 +0430

    Only users who have completed their account can send req ==> check profile added to this fragment after choose which one req want. app will check the profile if is completed will be start ti fill the req;

--- app/src/main/java/com/mohammadkz/bimix/Model/InsuranceResponse.java
+++ app/src/main/java/com/mohammadkz/bimix/Model/InsuranceResponse.java
@@ -10,24 +10,34 @@ public class InsuranceResponse {
     String date;
     @SerializedName("trackingCode")
     String trackingCode;
-    //    @SerializedName("link")
-//    String link;
+    @SerializedName("link")
+    String link;
     @SerializedName("payCode")
     String payCode;
     @SerializedName("result")
     String status;
+    @SerializedName("payStatus")
+    private String payed;
 
     public InsuranceResponse(String kind, String date, String trackingCode, String link, String payCode, String status) {
         this.kind = kind;
         this.date = date;
         this.trackingCode = trackingCode;
-//        this.link = link;
+        this.link = link;
         this.payCode = payCode;
         this.status = status;
     }
 
     public InsuranceResponse() {
 
+    }
+
+    public String getPayed() {
+        return payed;
+    }
+
+    public void setPayed(String payed) {
+        this.payed = payed;
     }
 
     public String getKind() {
@@ -54,13 +64,13 @@ public class InsuranceResponse {
         this.trackingCode = trackingCode;
     }
 
-//    public String getLink() {
-//        return link;
-//    }
-//
-//    public void setLink(String link) {
-//        this.link = link;
-//    }
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
 
     public String getPayCode() {
         return payCode;
