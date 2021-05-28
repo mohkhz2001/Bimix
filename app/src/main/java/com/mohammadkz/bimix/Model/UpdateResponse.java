4394b8ffe3492c0afd470b5059332ae0af30c178
mohammadmehdi khajehzadeh <mohammad.mehdi.khz@gmail.com>
2021-05-28 17:18:47 +0430

    for responce of update the table

---
+++ app/src/main/java/com/mohammadkz/bimix/Model/UpdateResponse.java
@@ -0,0 +1,13 @@
+package com.mohammadkz.bimix.Model;
+
+public class UpdateResponse {
+    String code;
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getCode() {
+        return code;
+    }
+}
