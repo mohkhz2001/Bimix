8233dd864ba6f25c0ad196de1b45c6f162d642dc
mohammadmehdi khajehzadeh <mohammad.mehdi.khz@gmail.com>
2021-06-10 20:34:06 +0430

    repetitious signup error added

--- app/src/main/java/com/mohammadkz/bimix/StaticFun.java
+++ app/src/main/java/com/mohammadkz/bimix/StaticFun.java
@@ -10,9 +10,33 @@ public class StaticFun {
 
     // error handler
     public static void alertDialog_connectionFail(Context context) {
+        try {
+            MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(context);
+            builder.setTitle(R.string.connection_failed_title);
+            builder.setMessage(R.string.connection_failed_text);
+            String positive = "بستن";
+
+            // have one btn ==> close
+            builder.setPositiveButton(positive, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+
+                    dialog.dismiss();
+                }
+            });
+
+            builder.show();
+        }catch (Exception e){
+            e.getMessage();
+        }
+
+    }
+
+    // error handler for input wrong phone number or pass
+    public static void alertDialog_error_login(Context context) {
         MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(context);
-        builder.setTitle(R.string.connection_failed_title);
-        builder.setMessage(R.string.connection_failed_text);
+        builder.setTitle(R.string.faileLogin_title);
+        builder.setMessage(R.string.faileLogin_text);
         String positive = "بستن";
 
         // have one btn ==> close
@@ -27,11 +51,10 @@ public class StaticFun {
         builder.show();
     }
 
-    // error handler for input wrong phone number or pass
-    public static void alertDialog_error_login(Context context) {
+    public static void alertDialog_error_repetitious(Context context){
         MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(context);
-        builder.setTitle(R.string.faileLogin_title);
-        builder.setMessage(R.string.faileLogin_text);
+        builder.setTitle("حساب کاربری");
+        builder.setMessage("از شماره تلفن دیگری استفاده کنید.");
         String positive = "بستن";
 
         // have one btn ==> close
