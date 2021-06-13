41b434e24f41fc498945bc71cad79a0cdc7000fa
mohammadmehdi khajehzadeh <mohammad.mehdi.khz@gmail.com>
2021-06-13 20:08:15 +0430

    check new notification is start app

--- app/src/main/java/com/mohammadkz/bimix/Activity/MainPageActivity.java
+++ app/src/main/java/com/mohammadkz/bimix/Activity/MainPageActivity.java
@@ -4,30 +4,63 @@ import androidx.annotation.IdRes;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.SystemClock;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.navigation.NavigationView;
+import com.google.gson.Gson;
+import com.mohammadkz.bimix.API.ApiConfig;
+import com.mohammadkz.bimix.API.AppConfig;
+import com.mohammadkz.bimix.Fragment.AboutFragment;
+import com.mohammadkz.bimix.Fragment.ContactFragment;
 import com.mohammadkz.bimix.Fragment.GetPriceFragment;
 import com.mohammadkz.bimix.Fragment.HistoryFragment;
 import com.mohammadkz.bimix.Fragment.IssuanceFragment;
 import com.mohammadkz.bimix.Fragment.PayFragment;
 import com.mohammadkz.bimix.Fragment.ProfileFragment;
+import com.mohammadkz.bimix.Model.CheckResult;
 import com.mohammadkz.bimix.Model.User;
+import com.mohammadkz.bimix.Notification.NotificationReceiver;
+import com.mohammadkz.bimix.Notification.PayActivity;
 import com.mohammadkz.bimix.R;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import static com.mohammadkz.bimix.Notification.App.CHANNEL_1_ID;
 
 public class MainPageActivity extends AppCompatActivity {
 
@@ -35,22 +68,28 @@ public class MainPageActivity extends AppCompatActivity {
     TextView title;
     View header;
     NavigationView nav_view;
-    TextView userName, userPoint;
+    TextView userName, userPoint, header_point;
     MaterialToolbar topAppBar;
     DrawerLayout drawer_layout;
     ImageView nav_image;
     private User user;
+    private NotificationManagerCompat notificationManager;
+    ApiConfig request;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
 
+        notificationManager = NotificationManagerCompat.from(this);
+        request = AppConfig.getRetrofit().create(ApiConfig.class);
+
         initViews();
         controllerViews();
         setUser();
         setValue();
         start();
+        checkServer();
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer_layout, R.string.openDrawerContentRes, R.string.closeDrawerContentRes);
         drawer_layout.addDrawerListener(toggle);
@@ -69,6 +108,7 @@ public class MainPageActivity extends AppCompatActivity {
         header = nav_view.getHeaderView(0);
         userName = header.findViewById(R.id.user_name);
         userPoint = header.findViewById(R.id.user_point);
+        header_point = header.findViewById(R.id.header_point);
 
     }
 
@@ -115,9 +155,7 @@ public class MainPageActivity extends AppCompatActivity {
                         break;
 
                     case R.id.nav_pay:
-                        setTitle("پرداخت بیمه نامه");
-                        PayFragment payFragment = new PayFragment(user);
-                        fragmentTransaction.replace(R.id.frameLayout, payFragment).commit();
+                        payFragment();
 
                         break;
                     case R.id.history:
@@ -132,6 +170,25 @@ public class MainPageActivity extends AppCompatActivity {
                         fragmentTransaction.replace(R.id.frameLayout, issuanceFragment);
                         fragmentTransaction.commit();
 
+                        break;
+
+                    case R.id.about:
+                        setTitle("درباره ما");
+                        AboutFragment aboutFragment = new AboutFragment();
+                        fragmentTransaction.replace(R.id.frameLayout, aboutFragment).commit();
+                        break;
+
+                    case R.id.contact:
+                        setTitle("ارتباط با ما");
+                        ContactFragment contactFragment = new ContactFragment();
+                        fragmentTransaction.replace(R.id.frameLayout, contactFragment).commit();
+                        break;
+
+                    case R.id.logout:
+                        removeSharedPreferences();
+                        finish();
+                        Intent intent = new Intent(MainPageActivity.this, StartUpActivity.class);
+                        startActivity(intent);
                         break;
                 }
 
@@ -160,9 +217,9 @@ public class MainPageActivity extends AppCompatActivity {
                 JSONObject jsonObject = new JSONObject(data);
                 user = new User();
                 user.setAuth(jsonObject.getString("auth"));
-                user.setID(jsonObject.getString("ID"));
                 user.setPhoneNumber(jsonObject.getString("phoneNumber"));
                 user.setName(jsonObject.getString("name"));
+                System.out.println();
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -177,7 +234,8 @@ public class MainPageActivity extends AppCompatActivity {
     //set value to the navigation
     private void setValue() {
         userName.setText(user.getName());
-        userPoint.setText(user.getPoint());
+        header_point.setText(user.getPoint());
+        header_point.setVisibility(View.VISIBLE);
     }
 
     public void profile() {
@@ -188,8 +246,78 @@ public class MainPageActivity extends AppCompatActivity {
         nav_view.setCheckedItem(R.id.nav_profile);
     }
 
-    public void setNavSelected(@IdRes int id){
+    public void setNavSelected(@IdRes int id) {
         nav_view.setCheckedItem(id);
+    }
+
+    private void sendOnChannel1(String code, String message) {
+
+
+        Intent intent = new Intent(MainPageActivity.this, PayActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        PendingIntent contentIntent = PendingIntent.getActivity(getApplicationContext(),
+                0, intent, 0);
+
+        Intent broadcastIntent = new Intent(getApplicationContext(), NotificationReceiver.class);
+        broadcastIntent.putExtra("payCode", code);
+
+
+        PendingIntent actionIntent = PendingIntent.getBroadcast(getApplicationContext(),
+                0, broadcastIntent, PendingIntent.FLAG_ONE_SHOT);
+
+        Notification notification = new NotificationCompat.Builder(getApplicationContext(), CHANNEL_1_ID)
+                .setSmallIcon(R.drawable.ic_done)
+                .setContentTitle("آماده ی پرداخت")
+                .setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .setColor(Color.BLUE)
+                .setContentIntent(contentIntent)
+                .setAutoCancel(true)
+                .setOnlyAlertOnce(true)
+                .build();
+
+        notificationManager.notify(1, notification);
+    }
+
+    private void checkServer() {
+        Call<List<CheckResult>> get = request.checkResult(user.getAuth());
+
+        get.enqueue(new Callback<List<CheckResult>>() {
+            @Override
+            public void onResponse(Call<List<CheckResult>> call, Response<List<CheckResult>> response) {
+                resultSearch(response.body());
+            }
+
+            @Override
+            public void onFailure(Call<List<CheckResult>> call, Throwable t) {
+                t.getMessage();
+            }
+        });
+
+    }
+
+    private void resultSearch(List<CheckResult> New) {
+
+        for (int i = 0; i < New.size(); i++) {
+            sendOnChannel1(New.get(i).getPayCode(), "کد پیگیری:" + New.get(i).getTrackingCode());
+        }
+
+    }
+
+    public void payFragment() {
+        setTitle("پرداخت بیمه نامه");
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        PayFragment payFragment = new PayFragment(user);
+        fragmentTransaction.replace(R.id.frameLayout, payFragment).commit();
+    }
+
+    private void removeSharedPreferences() {
+        SharedPreferences preferences = getSharedPreferences("userLogin", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.clear();
+        editor.commit();
     }
 
 }
